c6b60ffc5eb1f4df38d618179ff2f7879efc8acf
bdvela20 <bdvela20@gmail.com>
2024-05-28 07:03:12 +0000

    first commit new portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: netlify(),
+  i18n: {
+    defaultLocale: "es",
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      en: 'es',
+    }
+  }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'header.experience':'Experiencia',
+        'header.projects':'Proyectos',
+        'header.about':'Sobre mi',
+        'header.contact':'Contacto'
+    },
+    en: {
+        'header.experience':'Experience',
+        'header.projects':'Projects',
+        'header.about':'About me',
+        'header.contact':'Contact'
+    },
+} as const;
