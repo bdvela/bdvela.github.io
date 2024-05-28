31253e0851c6b2521e396a70a301a3a59f5da197
bdvela20 <bdvela20@gmail.com>
2024-05-28 23:43:32 +0000

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: netlify(),
+  osite: 'https://bdvela.github.io',
   i18n: {
     defaultLocale: "es",
     locales: ['es', 'en'],
